af35a6ca52ab90fbb34d51245f5fa6172f3a9ad4
carraes <carraeshb@gmail.com>
2022-12-13 10:41:43 -0300

    fix: login page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line global-require
+  plugins: [require('daisyui')],
+};
